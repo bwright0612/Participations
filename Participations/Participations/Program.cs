7e149f58bfe33726611895b4413d7c2254d7590b
Brock Wright <Brock.A.Wright@ou.edu>
2019-09-12 11:14:18 -0500

    I added stuff.

---
+++ Participations/Participations/Program.cs
@@ -0,0 +1,59 @@
+﻿//@author Brock Wright
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Participations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            //Just checking to see if it really just generates values that are either 1 or 2
+            //for (int i = 0; i < 50; i++)
+            //{
+
+            //}
+            int roll = rand.Next(1, 3);
+            int userGuess = 0;
+            Console.WriteLine("Call it in the air, heads or tails? >>");
+            string answer = Console.ReadLine();
+
+            if (answer == "heads")
+            {
+                userGuess = 1;
+            }
+            else if (answer == "tails")
+            {
+                userGuess = 2;
+            }
+            else
+            {
+                Console.WriteLine("Sorry invalid guess. Goodbye.");
+                Environment.Exit(0);
+            }
+
+            if (userGuess == roll)
+            {
+                Console.WriteLine($"Congratulations, it came up {answer} you guessed right!");
+            }
+            else
+            {
+                if(roll == 1)
+                {
+                    Console.WriteLine("Sorry, the coin came up heads");
+
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, the coin came up tails");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RandomGuess/RandomGuess/Program.cs
@@ -0,0 +1,72 @@
+﻿//Brock Wright
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CLASS SOLUTION.
+            Random rand = new Random();
+            Console.WriteLine("Please enter the minimum for your random number. >>");
+            int min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the maximum for your random number. >>");
+            int max = Convert.ToInt32(Console.ReadLine());
+
+            //If the loop starts here, we will generate a new random number every time.
+            int generatednumber = rand.Next(min, max + 1);
+
+            Console.WriteLine(generatednumber);
+            int ans = 0;
+            while (ans != generatednumber)
+            {
+                Console.WriteLine($"Please guess a number between {min} and {max}. >>");
+                string answer = Console.ReadLine();
+                if(Int32.TryParse(answer, out ans) == false)
+                {
+                    Console.WriteLine("Invalid input, please try again.");
+                }
+
+                ans = Convert.ToInt32(answer);
+
+                if (ans == generatednumber)
+                {
+                    Console.WriteLine("Congratulations, you guessed it!");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, try again.");
+                }
+            }
+            
+
+            Console.ReadKey();
+            //My Solution:
+            //Random rand = new Random();
+            //int roll = rand.Next(1, 100);
+            //Start:
+            //Console.WriteLine("Please guess a number between 1 and 100. >>");
+            //string answerAsString = Console.ReadLine();
+            //int answer = Convert.ToInt32(answerAsString);
+            
+
+
+            //if (roll == answer)
+            //{
+            //    Console.WriteLine("Congratulations! You correctly guessed the right number!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You did not guess correctly.");
+            //    goto Start;
+            //}
+
+            //Console.ReadKey();
+        }
+    }
+}
